0463bfbaf6a064f4b022e4567ee0b48a8900863b
victoria-vepreve <victoriav88@nycstudents.net>
2025-05-03 20:29:13 -0400

    Add files via upload

--- Main (1).java
+++ Main (1).java
@@ -6,5 +6,13 @@ public class Main{
         tr.addDailySteps(9000);
         tr.addDailySteps(5000);
         System.out.println(tr.averageSteps());
+
+        tr.addDailySteps(13000);
+        System.out.println(tr.activeDays());
+        System.out.println(tr.averageSteps());
+        tr.addDailySteps(23000);
+        tr.addDailySteps(1111);
+        System.out.println(tr.activeDays());
+        System.out.println(tr.averageSteps());
     }
 }
